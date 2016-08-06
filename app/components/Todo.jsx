d0e1b3d33f5a9146e7ab5084b66fa33d1d74fb4b
Martynas Kadiša <candylifter@gmail.com>
2016-08-06 19:05:48 +0300

    Add toggle support

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -2,10 +2,13 @@ var React = require('react');
 
 module.exports = React.createClass({
   render: function () {
-    var {id, text} = this.props;
+    var {id, text, completed} = this.props;
     return (
-      <div>
-        {id + '. ' + text}
+      <div onClick={() => {
+          this.props.onToggle(id);
+        }}>
+        <input type="checkbox" checked={completed}/>
+        {text}
       </div>
     )
   }
